4d72147bfe2f1150112aa48b6ab0e8e95a3afa22
Angela <angelalamluu@gmail.com>
2021-06-09 21:11:29 -0700

    commit tree fedeca325f26e464e58beccace0071cfff0c650d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(arr, string) {
+    return arr.filter( driver => 
+        driver.toLowerCase() === string.toLowerCase()
+    )
+}
+
+function fuzzyMatch(arr, string) {
+    return arr.filter( driver =>
+        driver.toLowerCase().indexOf(string.toLowerCase()) === 0
+    )
+}
+
+function matchName(arr, string) {
+    return arr.filter( driver => driver.name === string)
+}
